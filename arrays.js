132601970f0f3ac2bac59183346459014d6fadf0
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-10-25 13:38:30 +0000

    update

--- arrays.js
+++ arrays.js
@@ -1,11 +1,30 @@
-const strings = ['a', 'b', 'c', 'd', 'e']
+const strings = ["a", "b", "c", "d", "e"];
 
-strings[0] //O(1)
+strings[0]; //O(1)
 
-strings.push('g'); //O(1)
+strings.push("g"); //O(1)
 
-strings.unshift('1') //O(n)
+strings.pop(); //O(1)
 
-strings.splice(1,2,'2') //O(n)
+strings.unshift("1"); //O(n)
+
+strings.splice(1, 2, "2"); //O(n)
 
 console.log(strings);
+
+
+// OPTIONAL CLASSES
+
+// class Player {
+//   constructor(name, type) {
+//     this.name = name;
+//     this.type = type;
+//   }
+//   printName() {
+//     console.log(this.name);
+//   }
+// }
+
+// let player1 = new Player('Samuel', 'Player1');
+
+// player1.printName();
